14ef142850f96ce1dd6a0479bfcfba2202118059
mdan22 <mdan22@proton.me>
2024-04-27 17:35:52 +0200

    add 'web and express' for 3a

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,8 @@
-const http = require('http')
+// const http = require('http')
 // import http from 'http' // same logic, different syntax
+
+const express = require('express')
+const app = express()
 
 let notes = [
     {
@@ -19,10 +22,35 @@ let notes = [
       }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end(JSON.stringify(notes))
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+    // Express automatically sets the value of the
+    // Content-Type header to be text/html. The status
+    // code of the response defaults to 200.
 })
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+    // response.json sends the notes array that was passed
+    // to it as a JSON formatted string. Express
+    // automatically sets the Content-Type header with the
+    // appropriate value of application/json.
+})
+
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, {'Content-Type': 'text/plain'})
+//     response.end(JSON.stringify(notes))
+// })
+
+// In the earlier version where we were only using Node, we
+// had to transform the data into the JSON formatted string
+// with the JSON.stringify method.
+
+// With Express, this is no longer required, because this
+// transformation happens automatically.
+
+// It's worth noting that JSON is a string and not a
+// JavaScript object like the value assigned to notes.
 
 const PORT = 3001
 app.listen(PORT)
