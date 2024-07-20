7d9c89c5345af0d280fbf7be017ea9b459143731
mdan22 <mdan22@proton.me>
2024-07-20 22:20:45 +0200

    add 'Same origin policy and CORS' for 3b + rm old node_modules

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 // Middleware  are functions that can be used for handling request and response objects.
