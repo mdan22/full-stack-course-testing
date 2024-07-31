fdb1de6208a8a7d4325d8d4683173f68cfddcb74
mdan22 <mdan22@proton.me>
2024-07-31 21:17:01 +0200

    add 'Connecting the backend to a database' for 3c

--- part3/index.js
+++ part3/index.js
@@ -1,10 +1,33 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
+
+const url = process.env.MONGODB_URI
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+// this method transforms the _id property (which is actually an object)
+// into a string just to be safe and to prevent issues in the future
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 // Middleware  are functions that can be used for handling request and response objects.
 
@@ -47,11 +70,13 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+  Note.find({}).then(notes => {
     // response.json sends the notes array that was passed
     // to it as a JSON formatted string. Express
     // automatically sets the Content-Type header with the
     // appropriate value of application/json.
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
