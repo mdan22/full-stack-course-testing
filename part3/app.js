f5b43225afd800ca4b3ba6ebba2b11227e91d3fe
mdan22 <mdan22@proton.me>
2024-09-12 12:02:37 +0200

    add 'Note on exports' for 4a

--- part3/app.js
+++ part3/app.js
@@ -5,6 +5,14 @@ const cors = require('cors')
 const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+
+// alternatively we could destructure the functions to their own variables
+// this second way of exporting may be preferable if only a small portion
+// of the exported functions are used in a file
+// const { info, error } = require('./utils/logger')
+// info('message')
+// error('error message')
+
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
