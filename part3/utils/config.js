db102007c0361f2dda8e456dbfdc7c667365a8d2
mdan22 <mdan22@proton.me>
2024-09-17 18:29:43 +0200

    add 'Test environment' for 4b

--- part3/utils/config.js
+++ part3/utils/config.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
 
-module.exports = {
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
+
+const config = {
   MONGODB_URI,
   PORT
 }
+
+module.exports = config
