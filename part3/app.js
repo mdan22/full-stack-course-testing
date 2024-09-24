bb5ede1b6b03b72daab68bf82d7a8e08f4664ceb
mdan22 <mdan22@proton.me>
2024-09-24 15:13:06 +0200

    add setup for 4d

--- part3/app.js
+++ part3/app.js
@@ -9,6 +9,7 @@ const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login') // add router for login code
 const middleware = require('./utils/middleware')
 
 const logger = require('./utils/logger')
@@ -43,6 +44,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 // the middleware for handling unsupported
 // routes (unknown endpoint) is next to the last middleware
---
+++ part3/controllers/login.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken') // needed to generate the tokens
+const bcrypt = require('bcrypt') // needed to encrypt passwords / tokens
+const loginRouter = require('express').Router() // needed to handle the post route (to log in)
+const User = require('../models/user') // needed to do asynchronous db operations like User.find({}) or User.save()
+
+// url will (probably) be /api/login
+
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body // destructuring
+
+  const user = await User.findOne({ username }) // check if username exists in db
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash) // check if password is correct
+
+  // if either user or password is not correct, respond with error message
+  if(!(user && passwordCorrect)) {
+    response.status(401).json({ // unauthorized
+      error: 'invalid username or password'
+    })
+  }
+
+  // generate token for user
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  }
+  // the created token contains the username and the user id in a digitally signed form
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  response
+    .status(200) // ok
+    // apparently we need to send token, username and name in the response
+    .send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
