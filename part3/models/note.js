87940ee1e808a2e8d1bfb9d8c2b7c65d58c29c4d
mdan22 <mdan22@proton.me>
2024-09-28 10:50:28 +0200

    fix project structure: add backend of part3 state into part3 dir

---
+++ part3/models/note.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+// we log the url as DB address instead of hardcoding it
+console.log('connecting to ', url)
+
+// logs a message to the console about the
+// success status when trying to connect
+mongoose.connect(url)
+  .then( () => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  // The content field is now required to be at least
+  // five characters long and it is set as required,
+  // meaning that it can not be missing.
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  important: Boolean,
+})
+
+// this method transforms the _id property (which is actually an object)
+// into a string just to be safe and to prevent issues in the future
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
---
+++ part3/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+// we can pass database user password with the command
+// node mongo.js yourPassword
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://mdan22:${password}@cluster0.x1k0o1c.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+// establish the connection to the database
+mongoose.connect(url)
+
+// define the schema for a note and the matching model
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// // create a new note object with the help of the Note model
+// const note = new Note({
+//   content: 'Mongoose makes things easy',
+//   important: true,
+// })
+
+// // call save method which saves the object to the database
+// // and close the connection to end the execution of this code
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+// retrieve objects from the DB with the find method
+// the parameter of find() expresses search conditions
+// so an empty object {} results in getting all notes stored in the notes collection
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
